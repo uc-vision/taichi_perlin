8f26a514ebcabf037fb88199f83ac4e8c1c7ddda
harry1576 <harrydobbs87@gmail.com>
2024-02-05 14:48:51 +1300

    updated beartypign imports to fix PEP 585 depreceation warning

--- taichi_perlin/dropout_3d.py
+++ taichi_perlin/dropout_3d.py
@@ -5,82 +5,90 @@ import numpy as np
 from taichi_perlin.perlin_3d import NoiseGenerator3D
 import torch
 from beartype import beartype
+from beartype.typing import Tuple
 
 RangeValue = Tuple[float, float] | float
 
+
 @beartype
-@dataclass 
+@dataclass
 class DropoutParams:
-  # (range) controls the scale/base frequency of noise - larger = bigger areas, smaller
-  noise_scale: RangeValue
-  octaves: int
+    # (range) controls the scale/base frequency of noise - larger = bigger areas, smaller
+    noise_scale: RangeValue
+    octaves: int
 
-  # controls the frequency of the noise 0.5 - uniform dropout 2.0 smooth coherent dropout
-  freq_multiplier: float  
+    # controls the frequency of the noise 0.5 - uniform dropout 2.0 smooth coherent dropout
+    freq_multiplier: float
 
-  dropout: RangeValue # (range) proportion of points to drop completely
-  peturb: RangeValue  # (range) proportion of points to peturb
+    dropout: RangeValue  # (range) proportion of points to drop completely
+    peturb: RangeValue  # (range) proportion of points to peturb
 
-  # controls the frequency which the peturbed directions change
-  # use smaller number to give 0 mean noise, larger to give biased noise
-  peturb_bias: float  
-  peturb_distance: RangeValue # (range) controls the magnitude of the peturbation
+    # controls the frequency which the peturbed directions change
+    # use smaller number to give 0 mean noise, larger to give biased noise
+    peturb_bias: float
+    peturb_distance: RangeValue  # (range) controls the magnitude of the peturbation
 
-  def _randomize(self, x):
-    if isinstance(x, Tuple):
-      return np.random.uniform(x[0], x[1])
-    else:
-      return x
-    
-  def _log_randomize(self, x):
-    if isinstance(x, Tuple):
-      return np.exp(np.random.uniform(np.log(x[0]), np.log(x[1])))
-    else:
-      return x
+    def _randomize(self, x):
+        if isinstance(x, Tuple):
+            return np.random.uniform(x[0], x[1])
+        else:
+            return x
 
-  def randomize(self) -> 'DropoutParams':
-    return replace(self, 
-                  noise_scale=self._log_randomize(self.noise_scale),
-                  dropout=self._randomize(self.dropout),
-                  peturb=self._randomize(self.peturb),
-                  peturb_distance=self._randomize(self.peturb_distance)
-                )
-  
+    def _log_randomize(self, x):
+        if isinstance(x, Tuple):
+            return np.exp(np.random.uniform(np.log(x[0]), np.log(x[1])))
+        else:
+            return x
 
-    
+    def randomize(self) -> "DropoutParams":
+        return replace(
+            self,
+            noise_scale=self._log_randomize(self.noise_scale),
+            dropout=self._randomize(self.dropout),
+            peturb=self._randomize(self.peturb),
+            peturb_distance=self._randomize(self.peturb_distance),
+        )
+
+
 class PointDropout:
-  def __init__(self, params: DropoutParams):
-    self.params = params
+    def __init__(self, params: DropoutParams):
+        self.params = params
 
-    scales = [ 
-      (1/(2**i), 1/(params.freq_multiplier**i)) for i in range(params.octaves)]
+        scales = [
+            (1 / (2**i), 1 / (params.freq_multiplier**i)) for i in range(params.octaves)
+        ]
 
-    self.gen = NoiseGenerator3D(scales)
-    self.gen_vec = NoiseGenerator3D( [(params.peturb_bias, 1)])
+        self.gen = NoiseGenerator3D(scales)
+        self.gen_vec = NoiseGenerator3D([(params.peturb_bias, 1)])
 
-  def __call__(self, points: torch.Tensor, seed: int = None):
+    def __call__(self, points: torch.Tensor, seed: int = None):
 
-    if seed is None:
-      seed = np.random.randint(0, 2**32 - 1)
+        if seed is None:
+            seed = np.random.randint(0, 2**32 - 1)
 
-    params = self.params.randomize()
+        params = self.params.randomize()
 
-    noise = torch.zeros((points.shape[0],), dtype=torch.float32, device=points.device)
-    self.gen.sample_kernel(points, noise, seed=seed, base_scale=params.noise_scale)
+        noise = torch.zeros(
+            (points.shape[0],), dtype=torch.float32, device=points.device
+        )
+        self.gen.sample_kernel(points, noise, seed=seed, base_scale=params.noise_scale)
 
-    quantiles = torch.tensor([min(1, params.dropout + params.peturb), params.dropout]).to(device=points.device)
-    t_noisy, t_drop = torch.quantile(noise, quantiles)
+        quantiles = torch.tensor(
+            [min(1, params.dropout + params.peturb), params.dropout]
+        ).to(device=points.device)
+        t_noisy, t_drop = torch.quantile(noise, quantiles)
 
-    keep = torch.nonzero(noise > t_drop).squeeze()
-    points = points[keep]
+        keep = torch.nonzero(noise > t_drop).squeeze()
+        points = points[keep]
 
-    noise = 1 - torch.clamp((noise[keep] - t_drop) / (t_noisy - t_drop), 0, 1)
+        noise = 1 - torch.clamp((noise[keep] - t_drop) / (t_noisy - t_drop), 0, 1)
 
-    dirs = torch.zeros_like(points)
-    self.gen_vec.sample_vec_kernel(points, dirs, base_scale=params.noise_scale, seed=seed)
-    dirs /= torch.norm(dirs, dim=1).reshape(-1, 1)
+        dirs = torch.zeros_like(points)
+        self.gen_vec.sample_vec_kernel(
+            points, dirs, base_scale=params.noise_scale, seed=seed
+        )
+        dirs /= torch.norm(dirs, dim=1).reshape(-1, 1)
 
-    points = points + (dirs * noise.reshape(-1, 1)) * params.peturb_distance
+        points = points + (dirs * noise.reshape(-1, 1)) * params.peturb_distance
 
-    return points, keep
-  
+        return points, keep
--- taichi_perlin/perlin_2d.py
+++ taichi_perlin/perlin_2d.py
@@ -1,4 +1,5 @@
-from typing import Sequence, Tuple
+from typing import Sequence
+from beartype.typing import Tuple
 import taichi as ti
 import taichi.math as tm
 import numpy as np
@@ -8,59 +9,57 @@ from .noise_table import noise_table
 
 
 @ti.data_oriented
-class NoiseGenerator2D():
-  def __init__(self, scales:Sequence[Tuple[float, float]]):
-    self.table = noise_table(2)
-    self.scales = tuple(scales)
+class NoiseGenerator2D:
+    def __init__(self, scales: Sequence[Tuple[float, float]]):
+        self.table = noise_table(2)
+        self.scales = tuple(scales)
 
-  @ti.func
-  def grad_to(self, p:tm.vec2, i:tm.ivec2, seed:ti.i32) -> ti.f32:
-    v = self.table.rand_unit(i, seed)
-    return v.dot(p - ti.cast(i, ti.f32))
-    
-  @ti.func
-  def perlin_2d(self, p:tm.vec2, seed:ti.i32) -> ti.f32:
-    i = ti.floor(p, dtype=ti.i32)
-    f = p - i
-    u = f * f * (3 - 2 * f)
-    
-    a00 = self.grad_to(p, i + tm.ivec2(0, 0), seed)
-    a01 = self.grad_to(p, i + tm.ivec2(0, 1), seed)
-    a10 = self.grad_to(p, i + tm.ivec2(1, 0), seed)
-    a11 = self.grad_to(p, i + tm.ivec2(1, 1), seed)
+    @ti.func
+    def grad_to(self, p: tm.vec2, i: tm.ivec2, seed: ti.i32) -> ti.f32:
+        v = self.table.rand_unit(i, seed)
+        return v.dot(p - ti.cast(i, ti.f32))
 
-    return tm.mix(
-       tm.mix(a00, a01, u.y), tm.mix(a10, a11, u.y), u.x)  
+    @ti.func
+    def perlin_2d(self, p: tm.vec2, seed: ti.i32) -> ti.f32:
+        i = ti.floor(p, dtype=ti.i32)
+        f = p - i
+        u = f * f * (3 - 2 * f)
 
-  @ti.kernel
-  def image_kernel(self, image:ti.types.ndarray(dtype=ti.f32, ndim=2), scales:ti.template(), seed:ti.i32):
+        a00 = self.grad_to(p, i + tm.ivec2(0, 0), seed)
+        a01 = self.grad_to(p, i + tm.ivec2(0, 1), seed)
+        a10 = self.grad_to(p, i + tm.ivec2(1, 0), seed)
+        a11 = self.grad_to(p, i + tm.ivec2(1, 1), seed)
 
-    for i in ti.grouped(ti.ndrange(*image.shape)):
-      out = 0.0
-      for (scale, weight) in ti.static(scales):
-        p = tm.vec2(ti.f32(i.x), ti.f32(i.y)) / scale
-        out += self.perlin_2d(p, seed) * weight
-      
-      image[i] = out / 2 + 0.5
+        return tm.mix(tm.mix(a00, a01, u.y), tm.mix(a10, a11, u.y), u.x)
 
+    @ti.kernel
+    def image_kernel(
+        self,
+        image: ti.types.ndarray(dtype=ti.f32, ndim=2),
+        scales: ti.template(),
+        seed: ti.i32,
+    ):
 
+        for i in ti.grouped(ti.ndrange(*image.shape)):
+            out = 0.0
+            for scale, weight in ti.static(scales):
+                p = tm.vec2(ti.f32(i.x), ti.f32(i.y)) / scale
+                out += self.perlin_2d(p, seed) * weight
 
-
+            image[i] = out / 2 + 0.5
 
 
 if __name__ == "__main__":
-  import cv2
+    import cv2
 
-  ti.init(arch=ti.gpu, offline_cache=False)
-  np.random.seed(0)
+    ti.init(arch=ti.gpu, offline_cache=False)
+    np.random.seed(0)
 
-  scales = ((64, 1), (32, 1), (16, 0.5), (8, 0.25), (4, 0.125), (2, 0.0625))
+    scales = ((64, 1), (32, 1), (16, 0.5), (8, 0.25), (4, 0.125), (2, 0.0625))
 
-  image = np.zeros((512, 512), dtype=np.float32)
-  noise = NoiseGenerator2D(scales)
+    image = np.zeros((512, 512), dtype=np.float32)
+    noise = NoiseGenerator2D(scales)
 
-
-  noise.image_kernel(image, 0) 
-  cv2.imshow('image', image)
-  cv2.waitKey(0)
-
+    noise.image_kernel(image, 0)
+    cv2.imshow("image", image)
+    cv2.waitKey(0)
